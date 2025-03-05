10c4c9716764e976e6c6c3020c78d8a97e33b6fe
Cyube <lcslayer22@gmail.com>
2025-03-05 10:46:09 +0800

    Update register.php

---
+++ register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "inc/head.inc.php"; ?>
+    <title>Member Registration</title>
+</head>
+<body>
+    <?php include "inc/nav.inc.php"; ?>
+    <main class="container mt-5">
+        <h1>Member Registration</h1>
+        <p>For existing members, please go to the <a href="login.php">Sign In page</a>.</p>
+        <form action="process_register.php" method="post">
+            <div class="mb-3">
+                <label for="fname" class="form-label">First Name:</label>
+                <input required maxlength="45" type="text" id="fname" name="fname" class="form-control" placeholder="Enter first name">
+            </div>
+            <div class="mb-3">
+                <label for="lname" class="form-label">Last Name:</label>
+                <input required maxlength="45" type="text" id="lname" name="lname" class="form-control" placeholder="Enter last name" required>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input required maxlength="45" type="email" id="email" name="email" class="form-control" placeholder="Enter email" required>
+            </div>
+            <div class="mb-3">
+                <label for="pwd" class="form-label">Password:</label>
+                <input required maxlength="45" type="password" id="pwd" name="pwd" class="form-control" placeholder="Enter password" required>
+            </div>
+            <div class="mb-3">
+                <label for="pwd_confirm" class="form-label">Confirm Password:</label>
+                <input required maxlength="45" type="password" id="pwd_confirm" name="pwd_confirm" class="form-control" placeholder="Confirm password" required>
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" name="agree" id="agree" class="form-check-input" required>
+                <label class="form-check-label" for="agree">Agree to terms and conditions.</label>
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </main>
+    <?php include "inc/footer.inc.php"; ?>
+</body>
+</html>
