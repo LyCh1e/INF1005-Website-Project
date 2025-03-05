9e2f7756c36dfc1bf8b8b6a4ae0c30c667868de7
Cyube <lcslayer22@gmail.com>
2025-03-05 10:00:23 +0800

    added nav bar

--- INC/nav.inc.php
+++ INC/nav.inc.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-sm bg-secondary" id="home" data-bs-theme="dark">
-    <a class="navbar-brand" href="">
-      <img class="logo" src="" alt="LOGO" title="View larger image...">
+    <a class="navbar-brand" href="images/paw.jpg">
+      <img class="logo" src="images/paw.jpg" alt="LOGO" title="View larger image...">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
@@ -8,16 +8,25 @@
     <nav class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#home">Home</a>
+          <a class="nav-link" href="index.php#home">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#dogs"></a>
+          <a class="nav-link" href="index.php#dogs">Dogs</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#cats">Cats</a>
+          <a class="nav-link" href="index.php#cats">Cats</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="account.php">
+            <img class="account-image" src="images/account.png" alt="Account" title="Account">
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#register">Register</a>
+          <a class="nav-link" href="register.php" href="images/login.png">
+            <img class="login-image" src="images/login.png" alt="Login" title="Login/Register">
+          </a>
         </li>
       </ul>
     </nav>
