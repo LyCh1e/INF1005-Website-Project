93515d8010867379955947f766ae5237800f9be8
Cyube <lcslayer22@gmail.com>
2025-03-07 17:30:03 +0800

    Update head.inc.php

--- INC/head.inc.php
+++ INC/head.inc.php
@@ -1,7 +1,7 @@
 <!--Bootstrap CSS-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
   integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-<link rel="stylesheet" href="CSS/main.css" />
+<link rel="stylesheet" href="css/main.css" />
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?">
 
@@ -12,7 +12,7 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-<script defer src="JS/main.js"></script>
+<script defer src="js/main.js"></script>
 
 <!--Meta Tags-->
 <meta charset="UTF-8">
