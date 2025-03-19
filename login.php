73d32735aead0363dffab0f6f4d0fb1192658465
Alyssa <alyssakwek@gmail.com>
2025-03-19 13:37:37 +0800

    pamela

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
             <p>Existing members log in here. For new members, please go to the
                 <a href="register.php" style='color: rgb(0, 78, 74)'>Member Registration page</a>.
             </p>
-            <form action="process_login.php" method="post">
+            <form id="loginForm" method="post">
                 <div class="mb-3">
                     <label for="email" class="form-label">Email:</label>
                     <input required maxlength="45" type="email" id="email" name="email" class="form-control"
@@ -24,11 +24,12 @@
                     <label for="pwd" class="form-label">Password:</label>
                     <input required maxlength="45" type="password" id="pwd" name="pwd" class="form-control"
                         placeholder="Enter password" required>
+                    <div id="loginError" class="error-message">Account does not exist or password is incorrect.</div>
                 </div>
                 <div class="mb-3">
                     <button type="submit" class="btn"
                         style='background-color: rgb(0, 78, 74); color: white'>Submit</button>
-                <!-- Add this right after the Submit button in login.php -->
+                </div>
                 <div class="mb-3">
                     <p>Forgot your password? <a href="forgot_password.php" style='color: rgb(0, 78, 74)'>Reset it here</a>.</p>
                 </div>
@@ -38,6 +39,7 @@
         <?php endif; ?>
     </main>
     <?php include "inc/footer.inc.php"; ?>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
