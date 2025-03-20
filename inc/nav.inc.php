0bf6760205cc4aa9e5824db6299a6ac3b50141af
Cyube <lcslayer22@gmail.com>
2025-03-20 21:44:30 +0800

    just some proper indentation

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -34,22 +34,22 @@
         </ul>
         <ul class="navbar-nav">
             <?php if (isset($_SESSION['fname'])): ?>
-            <h1 class="welcome-text">Welcome, <?php echo $_SESSION['fname']; ?>!</h1>
-            <li class="nav-item">
-                <div style="display: inline-block; padding: 5px; white-space: nowrap;">
-                    <a href="logout.php" onclick="return confirmLogout();">
-                        <img class="logout-image"src="images/logout.png" width="60" height="50" alt="logouticon">
-                    </a>
-                </div>
-            </li>
+                <h1 class="welcome-text">Welcome, <?php echo $_SESSION['fname']; ?>!</h1>
+                <li class="nav-item">
+                    <div style="display: inline-block; padding: 5px; white-space: nowrap;">
+                        <a href="logout.php" onclick="return confirmLogout();">
+                            <img class="logout-image"src="images/logout.png" width="60" height="50" alt="logouticon">
+                        </a>
+                    </div>
+                </li>
             <?php else: ?>
                 <li class="nav-item">
-                <div style="display: inline-block; padding: 5px; white-space: nowrap;">
-                    <a href="login.php">
-                        <img class="account-image" src="images/account.png" alt="accounticon">
-                    </a>
-                </div>
-            </li>
+                    <div style="display: inline-block; padding: 5px; white-space: nowrap;">
+                        <a href="login.php">
+                            <img class="account-image" src="images/account.png" alt="accounticon">
+                        </a>
+                    </div>
+                </li>
             <?php endif; ?>
         </ul>
     </nav>
